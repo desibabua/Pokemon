3c0032a06795cb83417c1abfb955e59a3a5c60a7
ayush <58028488+desibabua@users.noreply.github.com>
2020-01-09 23:49:59 +0530

    modified design of cards

--- script.js
+++ script.js
@@ -2,14 +2,13 @@ const {appendFileSync} = require('fs');
 const content = require('./pokemon.json');
 
 
-const part1 = `<div class="pokeCard">\n<div class="cardFront">\n<h1>`
-const Part2 = `</h1>\n<hr>\n<div>\n<img class="header" src="`
-const Part3 = `" alt="image" height="250px">\n</div>\n<div class="power">\n`
-const Part4 = `</div>\n<h4>Level : `
+const part1 = `<div class="pokeCard">\n<div class="power">\n<div>\n<h1>`
+const Part2 = `</h1>\n</div>\n<div>\n`
+const Part3 = `</div>\n</div>\n<div>\n<div class="cardFront">\n<img class="header" src="`
+const Part4 = `" alt="image" height="220px">\n<h4>Level : `
 const Part5 = `</h4>\n<h4>nextLevel : `
-const Part6 = `</h4>\n</div>\n<div class="cardBack">\n<h1>`
-const Part7 = `</h1>\n<hr>\n<div>\n<h4>Description</h4>\n<p>`
-const Part8 = `</p>\n</div>\n</div>\n</div>`
+const Part6 = `</h4>\n</div>\n<div class="cardBack">\n<h4>Description</h4>\n<p>`
+const Part7 = `</p>\n</div>\n</div>\n<div><button>Description</button></div></div>`
 const Img1 = `<img src="./images/`
 const Img2 = `.png" alt="" height="30"></img>\n`
 
@@ -30,7 +29,7 @@ const getHtmlParts = function (content) {
 const getHtml = function (content) {
     const {name, url, powers, Level, nextLevel, description} = content;
     const Power = powers.map((power) => Img1 + power + Img2).join("")
-    return part1 + name + Part2 + url + Part3 + Power + Part4 + Level + Part5 + nextLevel + Part6 + name + Part7 + description + Part8
+    return part1 + name + Part2 + Power + Part3 + url + Part4 + Level + Part5 + nextLevel + Part6 + description + Part7
 }
 
 const append = function () {
